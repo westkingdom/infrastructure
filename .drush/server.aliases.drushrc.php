4d45de50c9880fc8a42e673a2cb8767017b75a9a
West Infrastructure <wk_infrastructure@broadbent.dreamhost.com>
2016-09-21 21:18:01 -0700

    Update and correct Drush aliases.

--- .drush/server.aliases.drushrc.php
+++ .drush/server.aliases.drushrc.php
@@ -1,6 +1,11 @@
 <?php
 
-$aliases["site5-uvps1-dev"] = array (
-  'remote-host' => 'server.westkingdom.org',
+$aliases['site5-uvps1'] = array(
+  'remote-host' => 'server.westkingdom.org', // 50.23.81.227
+  'remote-user' => 'wkadmin',
+);
+$aliases['site5-uvps1-dev'] = array(
+  'remote-host' => 'server.westkingdom.org', // 50.23.81.227
   'remote-user' => 'www-dev',
 );
+
--- .drush/wk.aliases.drushrc.php
+++ .drush/wk.aliases.drushrc.php
@@ -1,5 +1,28 @@
 <?php
 
+$aliases['stage'] = array (
+  'parent' => '@server.site5-uvps1-dev',
+  'root' => '/srv/www/stage.westkingdom.org/drupal',
+  'uri' => 'http://stage.westkingdom.org',
+  'target-command-specific' => array(
+    'sql-sync' => array(
+      'no-cache' => TRUE,
+      'enable' => array('devel', 'stage_file_proxy'),
+      'disable' => array('passwordless'),
+      'sanitize' => TRUE,
+      'sanitize-password' => '1066',
+      'permission' => array(
+        'authenticated user' => array(
+          'add' => array('access devel information', 'access environment indicator'),
+        ),
+        'anonymous user' => array(
+          'add' => 'access environment indicator',
+        ),
+      ),
+    ),
+  ),
+);
+
 $aliases['live'] = array (
   'parent' => '@server.site5-uvps1',
   'root' => '/srv/www/live.westkingdom.org/drupal',
